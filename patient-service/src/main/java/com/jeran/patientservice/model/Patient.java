f4064ff33d66a8e2a904c6b2c993fe853e235c1a
jeran15 <jeranmiranda0306@gmail.com>
2025-10-06 14:27:45 +0530

    Update the module for Crud Application

--- patient-service/src/main/java/com/jeran/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/jeran/patientservice/model/Patient.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Entity
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @NotNull
